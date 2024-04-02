0d4089e59a773eba95c2cbe5b8caa4c3694e93bd
arvind-mangalarapu <arvind@able.do>
2024-04-02 18:38:03 +0530

    32

---
+++ src/components/Display.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { count } from '../store/stores'
+
+let counter
+
+    count.subscribe((value)=> {
+        counter = value
+    })
+</script>
+
+
+<h2>Current count is {counter}</h2>
---
+++ src/components/Increment.svelte
@@ -0,0 +1,13 @@
+<script>
+import { count } from '../store/stores'
+
+function increment(){
+     count.update((value) =>{
+         return value + 1
+     })
+}
+
+</script>
+
+
+<button on:click={increment}>Increment</button>
